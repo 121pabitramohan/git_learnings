36b08fcfbd2dbd4d48cfc037d550092c6ab111f5
121pabitramohan <121pabitramohan@gmail.com>
2024-06-09 01:37:39 +0500

    modified again GitLearning.java

--- test/GitLearning.java
+++ test/GitLearning.java
@@ -4,6 +4,6 @@ public class GitLearning {
     public static void main(String[] args) {
         int  a =10;
         int b = 20;
-        System.out.println(a+b);
+        System.out.println("add two integers "+a+b);
     }
 }
